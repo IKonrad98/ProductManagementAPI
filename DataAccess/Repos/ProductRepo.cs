9ae0afc76f4429f21911c4706f28c127d9e82d11
IKonrad98 <vanya.konrad@gmail.com>
2025-03-23 23:29:29 +0100

    add product repo

---
+++ DataAccess/Repos/ProductRepo.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementAPI.DataAccess.Repos.Generic;
+using ProductManagementAPI.DataAccess.Repos.RepoInterface;
+
+namespace ProductManagementAPI.DataAccess.Repos;
+
+public class ProductRepo : GenericRepo<ProductEntity>, IProductRepo
+{
+    private readonly ProductManagementDb _context;
+
+    public ProductRepo(ProductManagementDb context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<ProductEntity>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await _context.Products.ToListAsync(cancellationToken);
+    }
+}
