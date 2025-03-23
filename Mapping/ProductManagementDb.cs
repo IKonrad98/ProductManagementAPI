35a91078f72511c36924009e8ec73ffcf6034f64
IKonrad98 <vanya.konrad@gmail.com>
2025-03-23 22:06:45 +0100

    add automapper and product profile

---
+++ Mapping/ProductManagementDb.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductManagementAPI.DataAccess.Models;
+
+namespace ProductManagementAPI.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<ProductEntity, ProductModel>().ReverseMap();
+        CreateMap<CreateProductModel, ProductEntity>();
+        CreateMap<UpdateProductModel, ProductEntity>().ReverseMap()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+        CreateMap<ProductEntity, UpdateProductModel>();
+    }
+}
